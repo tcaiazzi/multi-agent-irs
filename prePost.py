e7db956c62da5d5db28ce0e4846f395267531e8e
MatteoIng <matteofoschi10@gmail.com>
2023-11-10 11:26:48 +0100

    aggiornate post condizioni

--- prePost.py
+++ prePost.py
@@ -357,7 +357,16 @@ def postCondizioni(action,spazio,agent):
             spazio[agent][13] = 1
             spazio[agent][6] = 0
             # Tutti gli attacchi a 0 con una prob
-    
+            p = random.random()
+            if p < 0.3 :
+                # Scalare gli attacchi con una prob
+                spazio[agent][14] = 0
+                spazio[agent][15] = 0
+                spazio[agent][16] = 0
+                spazio[agent][17] = 0
+                spazio[agent][18] = 0
+                spazio[agent][19] = 0
+                spazio[agent][20] = 0
         # SystemStart
         elif action == 15 :
             spazio[agent][6] = 1
