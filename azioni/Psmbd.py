9e9d685e5fa517b7ec54b3acb61692a58363d117
MatteoIng <matteofoschi10@gmail.com>
2023-12-28 16:14:19 +0100

    ok un tradeoff trovato

--- azioni/Psmbd.py
+++ azioni/Psmbd.py
@@ -2,7 +2,7 @@ from azioneAsincrona import azioneAsincrona
 
 class Psmbd(azioneAsincrona):
     def __init__(self):
-        self.reward = (300,1,1)
+        self.reward = (80,1,1)
         self.tempoAttuazione = self.reward[0]
         self.tempoAttesa = self.reward[0]
 
