c7b3ad745fd9f8804f36cf047ca62d823b387221
MatteoIng <matteofoschi10@gmail.com>
2023-12-28 16:46:54 +0100

    ok

--- azioni/Pphpcgi.py
+++ azioni/Pphpcgi.py
@@ -2,7 +2,7 @@ from azioneAsincrona import azioneAsincrona
 
 class Pphpcgi(azioneAsincrona):
     def __init__(self):
-        self.reward = (100,1,1)
+        self.reward = (200,1,1)
         self.tempoAttuazione = self.reward[0]
         self.tempoAttesa = self.reward[0]
 
